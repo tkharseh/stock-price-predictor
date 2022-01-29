9935da4d8d9897f918d919b849362eb69fd2a7bc
tkharseh <tariq.kharseh@mail.utoronto.ca>
2022-01-29 11:04:37 -0500

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,10 +35,12 @@ export default function App() {
         <form class="flex" onSubmit={(e) => submit(e)}>
           <input style={{ width: 900 }} class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a stock symbol, e.g. 'AAPL'" type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}></input>
           <div class="pl-9 pr-9">
-            <button class="font-bold px-9 w-full border border-gray-300 text-white-900 text-sm rounded-lg w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 bg-black hover:bg-slate-700" onSubmit={submit}> <img class="w-5" src={logo} alt="my image"/> </button>
+            <button class="font-bold px-9 w-full border border-gray-300 text-white-900 text-sm rounded-lg w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 bg-black hover:bg-slate-700" onSubmit={submit}> <img class="w-5" src={logo} alt="my image" /> </button>
           </div>
         </form>
       </div>
+
+      <p class="flex items-center justify-center"> {submittedTerm} </p>
     </div>
   )
 }
--- keras_demo.py
+++ keras_demo.py
@@ -28,57 +28,57 @@ def perform_lstm(ticker):
     Y = []  # Want to predict future closing prices
     for x in X_trans:
         Y.append(x[0])
-    #
+    
     # We do an 85/15 split for training/testing
     split = int(len(X_trans) * 0.85)
     X_train, X_test = (X_trans[:split], X_trans[split:])
     Y_train, Y_test = (Y[:split], Y[split:])
-    #
+    
     time_step = 5
-    #
+    
     Xtrain_step, Xtest_step = ([], [])
     Ytrain_step, Ytest_step = ([], [])
-    #
+    
     for t in range(time_step, len(X_train)):
         Xtrain_step.append(X_train[t - time_step: t, : X_train.shape[1]])
         Ytrain_step.append(Y_train[t])
     for t in range(time_step, len(X_test)):
         Xtest_step.append(X_test[t - time_step: t, : X_test.shape[1]])
         Ytest_step.append(Y_test[t])
-    #
+    
     z = np.array(Ytrain_step[0])
     z = np.c_[z, np.zeros(z.shape)]
     scaler.inverse_transform(z)
-    #
+    
     Xtrain_step, Ytrain_step = (np.array(Xtrain_step), np.array(Ytrain_step))
     Xtrain_step = np.reshape(
     Xtrain_step, (Xtrain_step.shape[0], Xtrain_step.shape[1], Xtrain_step.shape[2]))
-    #
+    
     Xtest_step, Ytest_step = (np.array(Xtest_step), np.array(Ytest_step))
     Xtest_step = np.reshape(
     Xtest_step, (Xtest_step.shape[0], Xtest_step.shape[1], Xtest_step.shape[2]))
-    #
+    
     lstm_model = keras.models.Sequential()
     lstm_model.add(keras.layers.LSTM(4, input_shape=(
     Xtrain_step.shape[1], Xtrain_step.shape[2])))
-    #
+    
     lstm_model.add(keras.layers.Dense(1))
     lstm_model.compile(loss="mean_squared_error", optimizer="adam")
     lstm_model.fit(Xtrain_step, Ytrain_step, epochs=10, validation_data=(
     Xtest_step, Ytest_step), batch_size=10, verbose=1)
-    #
+    
     print(lstm_model.summary())
-    #
+    
     trainGuess = lstm_model.predict(Xtrain_step)
     testGuess = lstm_model.predict(Xtest_step)
-    #
+    
     trainGuess = np.c_[trainGuess, np.zeros(trainGuess.shape)]
     testGuess = np.c_[testGuess, np.zeros(testGuess.shape)]
-    #
+    
     # Invert the normalization
     trainGuess = scaler.inverse_transform(trainGuess)
     trainGuess = [guess[0] for guess in trainGuess]
-    #
+    
     testGuess = scaler.inverse_transform(testGuess)
     testGuess = [guess[0] for guess in testGuess]
 
