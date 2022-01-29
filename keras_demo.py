2ae94635a6fcbddd6c07f22af9e9036181532de8
Amielll <amielnurja@gmail.com>
2022-01-29 16:20:14 -0500

    Tuning hyperparameters

--- keras_demo.py
+++ keras_demo.py
@@ -1,4 +1,3 @@
-from tkinter.tix import Tree
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
@@ -9,7 +8,7 @@ import data_fetcher
 
 
 def perform_lstm(ticker):
-    requests = data_fetcher.StockDataRequest('10y', '1d')
+    requests = data_fetcher.StockDataRequest('2y', '1d')
     requests.add_tickers([ticker])
 
     response = data_fetcher.load_stock_data(
@@ -28,57 +27,60 @@ def perform_lstm(ticker):
     Y = []  # Want to predict future closing prices
     for x in X_trans:
         Y.append(x[0])
-    #
+
     # We do an 85/15 split for training/testing
-    split = int(len(X_trans) * 0.85)
+    split = int(len(X_trans) * 0.8)
     X_train, X_test = (X_trans[:split], X_trans[split:])
     Y_train, Y_test = (Y[:split], Y[split:])
-    #
+
     time_step = 5
-    #
+
     Xtrain_step, Xtest_step = ([], [])
     Ytrain_step, Ytest_step = ([], [])
-    #
+
     for t in range(time_step, len(X_train)):
         Xtrain_step.append(X_train[t - time_step: t, : X_train.shape[1]])
         Ytrain_step.append(Y_train[t])
     for t in range(time_step, len(X_test)):
         Xtest_step.append(X_test[t - time_step: t, : X_test.shape[1]])
         Ytest_step.append(Y_test[t])
-    #
-    z = np.array(Ytrain_step[0])
-    z = np.c_[z, np.zeros(z.shape)]
-    scaler.inverse_transform(z)
-    #
+
     Xtrain_step, Ytrain_step = (np.array(Xtrain_step), np.array(Ytrain_step))
     Xtrain_step = np.reshape(
-    Xtrain_step, (Xtrain_step.shape[0], Xtrain_step.shape[1], Xtrain_step.shape[2]))
-    #
+        Xtrain_step, (Xtrain_step.shape[0], Xtrain_step.shape[1], Xtrain_step.shape[2]))
+
     Xtest_step, Ytest_step = (np.array(Xtest_step), np.array(Ytest_step))
     Xtest_step = np.reshape(
-    Xtest_step, (Xtest_step.shape[0], Xtest_step.shape[1], Xtest_step.shape[2]))
-    #
+        Xtest_step, (Xtest_step.shape[0], Xtest_step.shape[1], Xtest_step.shape[2]))
+
     lstm_model = keras.models.Sequential()
-    lstm_model.add(keras.layers.LSTM(4, input_shape=(
-    Xtrain_step.shape[1], Xtrain_step.shape[2])))
-    #
+    lstm_model.add(keras.layers.LSTM(4, return_sequences=True, input_shape=(
+        Xtrain_step.shape[1], Xtrain_step.shape[2])))
+
+    lstm_model.add(keras.layers.Dropout(0.5))
+    #lstm_model.add(keras.layers.LSTM(4, return_sequences=True))
+    lstm_model.add(keras.layers.LSTM(4))
+    # lstm_model.add(keras.layers.Dropout(0.2))
+    # lstm_model.add(keras.layers.Dropout(0.2))
+    # lstm_model.add(keras.layers.Dense(1))
     lstm_model.add(keras.layers.Dense(1))
+
     lstm_model.compile(loss="mean_squared_error", optimizer="adam")
-    lstm_model.fit(Xtrain_step, Ytrain_step, epochs=10, validation_data=(
-    Xtest_step, Ytest_step), batch_size=10, verbose=1)
-    #
+    lstm_model.fit(Xtrain_step, Ytrain_step, epochs=100, validation_data=(
+        Xtest_step, Ytest_step), batch_size=10, verbose=1)
+
     print(lstm_model.summary())
-    #
+
     trainGuess = lstm_model.predict(Xtrain_step)
     testGuess = lstm_model.predict(Xtest_step)
-    #
+
     trainGuess = np.c_[trainGuess, np.zeros(trainGuess.shape)]
     testGuess = np.c_[testGuess, np.zeros(testGuess.shape)]
-    #
+
     # Invert the normalization
     trainGuess = scaler.inverse_transform(trainGuess)
     trainGuess = [guess[0] for guess in trainGuess]
-    #
+
     testGuess = scaler.inverse_transform(testGuess)
     testGuess = [guess[0] for guess in testGuess]
 
@@ -86,7 +88,6 @@ def perform_lstm(ticker):
 
 
 if __name__ == "__main__":
-    result = perform_lstm("MSFT")
+    result = perform_lstm("TSLA")
     print(result)
     print(len(result))
-
